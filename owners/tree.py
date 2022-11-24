9f3a44bbc54b37228f802f00a0d37c4404f72a41
G7r7 <72744587+G7r7@users.noreply.github.com>
2022-11-24 14:07:15 +0000

    Tree

--- owners/tree.py
+++ owners/tree.py
@@ -1,10 +1,17 @@
 from typing import List
 from find import Relation, find_relations_by_target_name
         
-class RelationTree(Relation):
+class RelationTree:
     def __init__(self, data: list, name: str):
         matches = find_relations_by_target_name(data, name)
         if len(matches) == 0:
             return
-        self = matches[0]
-        self.tree = RelationTree(data, self.origine)
+        self.root = matches[0]
+        self.list = RelationTreeList(data, self.root.origine)
+
+
+class RelationTreeList(list):
+    def __init__(self, data: list, name: str):
+        matches = find_relations_by_target_name(data, name)
+        for match in matches:
+            self.append(RelationTree(data, match.cible))
